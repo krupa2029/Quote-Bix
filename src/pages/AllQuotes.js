eeb2c31dfa647104d44e58fa8ab96d3e9c3bc351
krupa2029 <kjkotia10@gmail.com>
2021-11-26 21:58:25 +0530

    Managing Quote Detail page

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,11 +1,15 @@
 import QuoteList from "../components/quotes/QuoteList";
 
-const DUMMY_QUOTES =[
-    {id: 'q1', author:'Kru', text: 'Never Give Up' },
-    {id: 'q2', author:'Erica Jong', text: 'If you don’t risk anything, you risk even more!' },
-]
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Kru", text: "Never Give Up" },
+  {
+    id: "q2",
+    author: "Erica Jong",
+    text: "If you don’t risk anything, you risk even more!",
+  },
+];
 
-const AllQuotes =()=>{
-    return <QuoteList quotes={DUMMY_QUOTES} />
+const AllQuotes = () => {
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 export default AllQuotes;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,16 +1,31 @@
 import { Route, useParams } from "react-router";
-import { Fragment } from "react/cjs/react.production.min";
+import { Fragment } from "react";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import Comments from "../components/comments/Comments";
 
-import Comments from '../components/comments/Comments';
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Kru", text: "Never Give Up" },
+  {
+    id: "q2",
+    author: "Erica Jong",
+    text: "If you don’t risk anything, you risk even more!",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote){
+      return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>QuoteDetail page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
-          <Comments />
+        <Comments />
       </Route>
     </Fragment>
   );
