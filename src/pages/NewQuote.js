04a9dbfebdaeed54d7cfba00c3d9afa116a9b7d5
krupa2029 <kjkotia10@gmail.com>
2021-11-27 00:24:45 +0530

    Preventing possibly unwanted route transition Used: Prompt

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,13 @@
+import { useHistory } from "react-router";
 import QuoteForm from "../components/quotes/QuoteForm";
 
-const NewQuote =()=>{
-    const addQuoteHandler = quoteData => {
-        console.log(quoteData);
-    }
-    return <QuoteForm onAddQuote={addQuoteHandler} />
+const NewQuote = () => {
+  const history = useHistory();
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+
+    history.push('/quotes');
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 export default NewQuote;
