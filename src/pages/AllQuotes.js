ada69c947f444a291c51ee76c3b695079f7635c4
krupa2029 <kjkotia10@gmail.com>
2021-11-26 21:29:07 +0530

    Add: Form into NewQuote pages Add: Dummy Quotes in AllQuotes Page

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,4 +1,11 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES =[
+    {id: 'q1', author:'Kru', text: 'Never Give Up' },
+    {id: 'q2', author:'Erica Jong', text: 'If you don’t risk anything, you risk even more!' },
+]
+
 const AllQuotes =()=>{
-    return <h1>All Quotes page</h1>
+    return <QuoteList quotes={DUMMY_QUOTES} />
 };
 export default AllQuotes;
